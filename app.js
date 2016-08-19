6075f899c174c80ff2f63016abca037c35e0d0e0
quantumarchive <chrbruner9@gmail.com>
2016-08-19 13:57:48 -0700

    solved the sumArray problem using a for loop

--- app.js
+++ app.js
@@ -77,13 +77,24 @@ Test this function by hand in the console to get it working, and when you think 
 // Write your code here
 var testArray = [2,3,4]; //eslint-disable-line
 
-function sumArray(testArray){ //eslint-disable-line
-
+function sumArray(testArray) { //eslint-disable-line
+  var total = 0;
+  var mystring = '';
+  for (var i = 0; i < testArray.length; i++){
+    total = sum(total, testArray[i])[0];
+    if (i === (testArray.length - 1)) {
+      mystring = mystring.concat(testArray[i]);
+    } else {
+      mystring = mystring.concat(testArray[i],',');
+    }
+  }
+  mystring = mystring.concat(' was passed in as an array of numbers, and ', total, ' is their sum.');
+  return [total, mystring];
 }
 
 // Here is the test for sumArray(); uncomment it to run it
 
-// testSumArray(testArray);
+testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
